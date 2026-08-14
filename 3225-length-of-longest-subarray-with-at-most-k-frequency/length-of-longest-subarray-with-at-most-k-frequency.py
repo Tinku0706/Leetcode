a924787fc64a961dc58277e9f16de8dbbbce8b20
Rithvik Dandu <dandurithvik7@gmail.com>
2026-08-14 18:55:14 +0530

    Time: 233 ms (91.07%) | Memory: 35.3 MB (72.99%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -3,11 +3,14 @@ class Solution:
         freq={}
         left=ans=0
         for right in range(len(nums)):
-            freq[nums[right]]=freq.get(nums[right],0)+1
+            freq[nums[right]]=freq.get(nums[right],0)+1 
 
             while freq[nums[right]]>k:
                 freq[nums[left]]-=1
                 left+=1
             ans=max(ans,right-left+1)
         return ans 
-        
+
+#freq = {1: 2, 2: 1, 3: 1}-then when i add 4th elemnt(1)
+#freq becomes 1:2 means 2>2 while condiiton so i will remove leftmost elmt
+# Because when left moves forward, we're removing that element from the sliding window.  
