d620e770c2161558640769900999ac7337e4bb32
Rithvik Dandu <dandurithvik7@gmail.com>
2026-08-20 22:42:52 +0530

    Time: 7 ms (37.87%) | Memory: 19.2 MB (47.43%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,8 +1,8 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mp1={}
-        mp2={}
-        for x,y in zip(s,t):
+        mp1={}#mp1 asks:What character does this s character map to?
+        mp2={}   #mp2 asks: Has this t character already been taken by another s character?
+        for x,y in zip(s,t):  #takes e from s and a from t string (e,a)then(g,d)
             if x in mp1 and mp1[x]!=y:
                 return False
             if y in mp2 and mp2[y]!=x:
@@ -10,4 +10,5 @@ class Solution:
             mp1[x]=y
             mp2[y]=x
         return True
-        
+    
+     
