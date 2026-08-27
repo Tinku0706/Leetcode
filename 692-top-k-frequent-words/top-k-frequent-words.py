43258a680f17092286d4f1b533950383c3823fa5
Rithvik Dandu <dandurithvik7@gmail.com>
2026-08-27 13:38:33 +0530

    Update top-k-frequent-words.py
    second option ans

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -6,3 +6,14 @@ class Solution:
         return ls[:k]  #means "give me the top k elements" 
     #-mp[x] → higher frequency comes first
 #x → if frequencies are equal, alphabetical order
+# class Solution:
+#     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+#         mp=Counter(words)
+#         ls=[]
+#         mp=dict(sorted(mp.items(), key=lambda x: (-x[1], x[0])))
+#         for key,value in mp.items():
+#             if len(ls)<k:
+#                 ls.append(key)
+#             else:
+#                 break
+#         return ls
